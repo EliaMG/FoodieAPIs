3c0b14dfc7dd06b8feea8a67c0ef160f7d1ca44b
Elia <elgrenier@gmail.com>
2015-09-08 20:14:43 -0700

    basic rails setup with file mods and basic controllers

---
+++ spec/controllers/apis_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+# TIMJ_POP =    { cassette_name: "TIMJ_popular", record: :new_episodes }
+
+RSpec.describe ApisController, type: :controller do
+  # describe "interacting with the This Is My Jam Artist API", vcr: TIMJ_SEARCH do
+  #   before :each do
+  #     get :search, artist: "the knife"
+  #   end
+
+end
